f58702f10b0e06eb2721e3d5f457d8679b087e91
wuxianglong <wuxlpersonal@gmail.com>
2022-09-19 14:23:07 +0800

    test update

--- src/main/java/com/example/documentdemo/DocumentDemoApplication.java
+++ src/main/java/com/example/documentdemo/DocumentDemoApplication.java
@@ -3,6 +3,9 @@ package com.example.documentdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wuxianglong
+ */
 @SpringBootApplication
 public class DocumentDemoApplication {
 
